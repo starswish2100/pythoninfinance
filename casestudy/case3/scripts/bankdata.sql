1fdad7254d087d39d12c5fb66021645e0b0833e9
Tianshan <starswish2100@hotmail.com>
2020-11-28 14:12:36 +0000

    nk case'

--- casestudy/case3/scripts/bankdata.sql
+++ casestudy/case3/scripts/bankdata.sql
@@ -1,11 +1,10 @@
-# IMPORTANT
-# DATA TIME FORMAT 2020-12-02
-# SET SQL_SAFE_UPDATES = 0;
-# UPDATE citidata set Exposure = NULL WHERE Exposure = '';
-# REMOVE ',' FROM NUMBERS
-# BIGIN NOT INT
-# ALTER TABLE citidata ADD id INT AUTO_INCREMENT PRIMARY KEY;
-
-# SELECT id, Date, demo.citidata.`Desk Level 4` FROM demo.citidata order by demo.citidata.`Desk Level 4`
-
-
+SELECT 
+`Business Level 2`, 
+`Business Sub Level 3`, 
+`Risk Type`,`Date`, 
+sum(Exposure) as `Exposure Sum`, 
+null as 'Difference', 
+null as 'Alert' 
+FROM demo.bankdata 
+group by `Business Level 2`, `Business Sub Level 3`, `Risk Type`, `Date` 
+order by `Business Level 2`, `Business Sub Level 3`, `Risk Type`, `Date`;
